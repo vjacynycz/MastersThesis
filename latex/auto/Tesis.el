122100fe00d27eb926598eeda0a362d41c096439
Viktor Jacynycz Garcia <viktor@jacynycz.es>
2018-01-29 17:34:05 +0100

    background

--- latex/auto/Tesis.el
+++ latex/auto/Tesis.el
@@ -5,6 +5,16 @@
                      '(("book" "11pt" "a4paper" "twoside")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "lmargin=4cm" "rmargin=3cm") ("todonotes" "colorinlistoftodos" "textwidth=4cm" "shadow")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "config"
@@ -30,11 +40,11 @@
     "geometry"
     "todonotes")
    (TeX-add-symbols
+    '("code" 1)
+    '("itbf" 1)
     '("compilaApendice" 1)
     '("compilaCapitulo" 1)
     "ficherosBasicosTeXiS"
-    "ficherosBasicosTexto")
-   (LaTeX-add-environments
-    '("SubFloat" LaTeX-env-args ["argument"] 1)))
+    "ficherosBasicosTexto"))
  :latex)
 
