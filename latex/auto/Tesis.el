57cb6ec9d81c1ba713ed007d079b4235bddb15bd
Viktor Jacynycz Garcia <viktor@jacynycz.es>
2018-01-30 16:45:15 +0100

    draft

--- latex/auto/Tesis.el
+++ latex/auto/Tesis.el
@@ -6,15 +6,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "lmargin=4cm" "rmargin=3cm") ("todonotes" "colorinlistoftodos" "textwidth=4cm" "shadow")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "config"
@@ -37,11 +37,14 @@
     "book"
     "bk11"
     "TeXiS/TeXiS"
+    "blindtext"
     "geometry"
     "todonotes")
    (TeX-add-symbols
     '("code" 1)
     '("itbf" 1)
+    '("bb" 1)
+    '("lip" 0)
     '("compilaApendice" 1)
     '("compilaCapitulo" 1)
     "ficherosBasicosTeXiS"
